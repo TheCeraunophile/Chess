81efd284932729c566586b2fb5f944dc1ccc6dcd
Shahrabi.Soheil <shahrabi.soheil@yahoo.com>
2022-08-05 18:44:07 +0430

    Create initial structure

---
+++ Piece.py
@@ -0,0 +1,26 @@
+class Piece:
+    pass
+
+
+class Knight(Piece):
+    pass
+
+
+class King(Piece):
+    pass
+
+
+class Rook(Piece):
+    pass
+
+
+class Bishop(Piece):
+    pass
+
+
+class Queen(Piece):
+    pass
+
+
+class Pawn(Piece):
+    pass
