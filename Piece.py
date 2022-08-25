c8a902668d82db2b8b2274ce51360e639c980e2f
Shahrabi.Soheil <shahrabi.soheil@yahoo.com>
2022-08-25 21:35:20 +0430

    Make initialization of pieces simpler

--- Piece.py
+++ Piece.py
@@ -1,19 +1,18 @@
-from Player import Player
 from SetPin import king_knight, rook_bishop_queen, get_pawn
 
 
 class Piece:
-    def __init__(self, owner: Player):
+    def __init__(self, owner: int):
         self.owner = owner
         self.name = ''
         self.shape = ''
-        self.weight = 10
+        self.weight = 0
 
-    def check_move(self, board, src: tuple, check_pinned):
+    def check_move(self, board, src: tuple, check):
         pass
 
     def __eq__(self, other):
-        if other.owner.name == self.owner.name and self.name == other.name:
+        if other.owner == self.owner and self.name == other.name:
             return True
         return False
 
@@ -21,67 +20,61 @@ class Piece:
         return hash(self.owner) + hash(self.name)
 
 
-class Knight(Piece):
-    def __init__(self, owner: Player):
-        super().__init__(owner)
-        self.name = 'WHITE KNIGHT' if owner.name == 'WHITE' else 'BLACK KNIGHT'
-        self.shape = u'\u265E' if owner.name == 'WHITE' else u'\u2658'
-        self.weight = 30
-
-    def check_move(self, board, src: tuple, check_pinned):
-        return king_knight(board, src, 'knight')
-
-
 class King(Piece):
-    def __init__(self, owner: Player):
+    def __init__(self, owner: int):
         super().__init__(owner)
-        self.name = 'WHITE KING' if owner.name == 'WHITE' else 'BLACK KING'
-        self.shape = u'\u265A' if owner.name == 'WHITE' else u'\u2654'
-        self.weight = 900
+        self.name, self.shape = ('WHITE KING', u'\u265A') if owner == 0 else ('BLACK KING', u'\u2654')
+        self.weight = 12
 
-    def check_move(self, board, src: tuple, check_pinned):
-        return king_knight(board, src, 'king')
-
-
-class Rook(Piece):
-    def __init__(self, owner: Player):
-        super().__init__(owner)
-        self.name = 'WHITE ROOK' if owner.name == 'WHITE' else 'BLACK ROOK'
-        self.shape = u'\u265C' if owner.name == 'WHITE' else u'\u2656'
-        self.weight = 50
-
-    def check_move(self, board, src: tuple, check_pinned):
-        return rook_bishop_queen(board, src, 'rook')
-
-
-class Bishop(Piece):
-    def __init__(self, owner: Player):
-        super().__init__(owner)
-        self.name = 'WHITE BISHOP' if owner.name == 'WHITE' else 'BLACK BISHOP'
-        self.shape = u'\u265D' if owner.name == 'WHITE' else u'\u2657'
-        self.weight = 30
-
-    def check_move(self, board, src: tuple, check_pinned):
-        return rook_bishop_queen(board, src, 'bishop')
+    def check_move(self, board, src: tuple, check):
+        return king_knight(board, src, 12)
 
 
 class Queen(Piece):
-    def __init__(self, owner: Player):
+    def __init__(self, owner: int):
         super().__init__(owner)
-        self.name = 'WHITE QUEEN' if owner.name == 'WHITE' else 'BLACK QUEEN'
-        self.shape = u'\u265B' if owner.name == 'WHITE' else u'\u2655'
-        self.weight = 90
+        self.name, self.shape = ('WHITE QUEEN', u'\u265B') if owner == 0 else ('BLACK QUEEN', u'\u2655')
+        self.weight = 9
 
-    def check_move(self, board, src: tuple, check_pinned):
-        return rook_bishop_queen(board, src, 'queen')
+    def check_move(self, board, src: tuple, check):
+        return rook_bishop_queen(board, src, 9, check)
+
+
+class Rook(Piece):
+    def __init__(self, owner: int):
+        super().__init__(owner)
+        self.name, self.shape = ('WHITE ROOK', u'\u265C') if owner == 0 else ('BLACK ROOK', u'\u2656')
+        self.weight = 5
+
+    def check_move(self, board, src: tuple, check):
+        return rook_bishop_queen(board, src, 5, check)
+
+
+class Knight(Piece):
+    def __init__(self, owner: int):
+        super().__init__(owner)
+        self.name, self.shape = ('WHITE KNIGHT', u'\u265E') if owner == 0 else ('BLACK KNIGHT', u'\u2658')
+        self.weight = 3
+
+    def check_move(self, board, src: tuple, check):
+        return king_knight(board, src, 3)
+
+
+class Bishop(Piece):
+    def __init__(self, owner: int):
+        super().__init__(owner)
+        self.name, self.shape = ('WHITE BISHOP', u'\u265D') if owner == 0 else ('BLACK BISHOP', u'\u2657')
+        self.weight = 3
+
+    def check_move(self, board, src: tuple, check):
+        return rook_bishop_queen(board, src, 3, check)
 
 
 class Pawn(Piece):
-    def __init__(self, owner: Player):
+    def __init__(self, owner: int):
         super().__init__(owner)
-        self.name = 'WHITE PAWN' if owner.name == 'WHITE' else 'BLACK PAWN'
-        self.shape = u'\u265F' if owner.name == 'WHITE' else u'\u2659'
-        self.weight = 10
+        self.name, self.shape = ('WHITE PAWN', u'\u265F') if owner == 0 else ('BLACK PAWN', u'\u2659')
+        self.weight = 1
 
-    def check_move(self, board, src: tuple, check_pinned):
-        return get_pawn(board, src)
+    def check_move(self, board, src: tuple, check):
+        return get_pawn(board, src, check)
